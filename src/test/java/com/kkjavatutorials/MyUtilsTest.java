6e0f47a625b56f045a185aa6fed61870028ce168
kkjavatutorials <kkjavatutorials@gmail.com>
2020-08-24 16:58:42 +0530

    First commit

---
+++ src/test/java/com/kkjavatutorials/MyUtilsTest.java
@@ -0,0 +1,37 @@
+package com.kkjavatutorials;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MyUtilsTest {
+
+	
+	@BeforeEach
+	public void setUp() {
+		
+	}
+	
+	
+	@AfterEach
+	public void tearDown() {
+		
+	}
+
+	
+	@Test
+	public void testname() {
+		
+		
+	}
+	
+	
+	@Test
+	public void test2() {
+		
+		
+	}
+
+
+
+}
